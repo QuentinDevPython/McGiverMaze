8ce5d3e8e37d5feed14b2887a0f4f28ef99ffdb0
QuentinDevPython <quentin.barthelemy.01@gmail.com>
2020-09-24 15:14:28 +0200

    fix(all) new cleaner code [bugfix-McGiver]

--- maze_map.py
+++ maze_map.py
@@ -2,9 +2,11 @@ class Map:
 
 	WIDTH = 15
 	LENGHT = 15
-	index = 0
 
 	maze_grid = []
+
+	index = 0
+
 	for line_maze_grid in range(LENGHT):
 		maze_grid.append([0] * WIDTH)
 
@@ -24,10 +26,56 @@ class Map:
 		for column_maze_grid in range(Map.WIDTH):
 			for line_maze_grid in range(Map.LENGHT):
 				if Map.maze_grid[column_maze_grid][line_maze_grid] == "1":
-					return line_maze_grid,column_maze_grid
+					position = (line_maze_grid,column_maze_grid)
+					return position
 
 	def get_position_Guardian():
 		for column_maze_grid in range(Map.WIDTH):
 			for line_maze_grid in range(Map.LENGHT):
 				if Map.maze_grid[column_maze_grid][line_maze_grid] == "2":
-					return line_maze_grid,column_maze_grid
+					position = (line_maze_grid,column_maze_grid)
+					return position
+
+	def take_object(self, movement):
+
+		position = Map.get_position_player()
+
+		if movement == "right" and Map.maze_grid[position[1]][position[0]+1] == "3":
+			Map.maze_grid[position[1]][position[0]] = "0"
+			Map.maze_grid[position[1]][position[0]+1] = "1"
+			return True
+
+		if movement == "left" and Map.maze_grid[position[1]][position[0]-1] == "3":
+			Map.maze_grid[position[1]][position[0]] = "0"
+			Map.maze_grid[position[1]][position[0]-1] = "1"
+			return True
+
+		if movement == "up" and Map.maze_grid[position[1]-1][position[0]] == "3":
+			Map.maze_grid[position[1]][position[0]] = "0"
+			Map.maze_grid[position[1]-1][position[0]] = "1"
+			return True
+
+		if movement == "down" and Map.maze_grid[position[1]+1][position[0]] == "3":
+			Map.maze_grid[position[1]][position[0]] = "0"
+			Map.maze_grid[position[1]+1][position[0]] = "1"
+			return True
+
+	def player_next_square(self, allowed_movements, movement):
+
+		position = Map.get_position_player()
+
+		if allowed_movements[0] == True and movement == "right":
+			Map.maze_grid[position[1]][position[0]] = "0"
+			Map.maze_grid[position[1]][position[0]+1] = "1"
+
+		if allowed_movements[1] == True and movement == "left":
+			Map.maze_grid[position[1]][position[0]] = "0"
+			Map.maze_grid[position[1]][position[0]-1] = "1"
+
+		if allowed_movements[2] == True and movement == "up":
+			Map.maze_grid[position[1]][position[0]] = "0"
+			Map.maze_grid[position[1]-1][position[0]] = "1"
+
+		if allowed_movements[3] == True and movement == "down":
+			Map.maze_grid[position[1]][position[0]] = "0"
+			Map.maze_grid[position[1]+1][position[0]] = "1"
